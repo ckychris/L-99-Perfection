5d41fc37713c5b1f72753ea0038ecc969c836904
ckychris <ezreal223@gmail.com>
2015-09-30 14:39:56 +1000

    Solved P1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,32 @@
+import java.util.ArrayList;
+
 import solutions.*;
 
 public class Main {
 
+    private static ArrayList<String> list;
+
     public Main() {
-	// TODO Auto-generated constructor stub
+
     }
 
     public static void main(String[] args) {
+	// construct new list
+	list = new ArrayList<String>();
+	list.add("A");
+	list.add("B");
+	list.add("C");
+	list.add("D");
+	// initiate new Algorithm
 	Algorithm al = new Algorithm();
-	System.out.print(al.toString());
+	System.out.println(al.toString());
+	System.out.println();
+	// output list item
+	for (String i : list)
+	    System.out.printf("%s ", i);
+	System.out.println();
+	// 1-99
+	System.out.println("This last index of arrayList is: " + al.lastIndex(list));
     }
 
 }
--- src/solutions/Algorithm.java
+++ src/solutions/Algorithm.java
@@ -1,19 +1,20 @@
 package solutions;
 
+import java.util.ArrayList;
+
 public class Algorithm {
 
     public Algorithm() {
 	// TODO Auto-generated constructor stub
     }
 
-    // P01 (*) Find the last box of a list.
     public String toString() {
-	return "Algorithm builded";
+	return "Algorithm builded.";
     }
 
-    public String lastIndex(String[] array) {
-	return null;
-
+    // P01 (*) Find the last box of a list.
+    public String lastIndex(ArrayList<String> list) {
+	return list.get(list.size()-1);
     }
 
 }
