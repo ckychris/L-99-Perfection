9bc0a0a57b74878273bc78c5cc778cbb8cdaa7cb
ckychris <ezreal223@gmail.com>
2015-09-30 14:25:46 +1000

    Basic Class constructed

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import solutions.*;
+
+public class Main {
+
+    public Main() {
+	// TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+	Algorithm al = new Algorithm();
+	System.out.print(al.toString());
+    }
+
+}
---
+++ src/solutions/Algorithm.java
@@ -0,0 +1,19 @@
+package solutions;
+
+public class Algorithm {
+
+    public Algorithm() {
+	// TODO Auto-generated constructor stub
+    }
+
+    // P01 (*) Find the last box of a list.
+    public String toString() {
+	return "Algorithm builded";
+    }
+
+    public String lastIndex(String[] array) {
+	return null;
+
+    }
+
+}
